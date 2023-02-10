cc212880bb4e7954a86b454e5d85b6516538eed6
Scala Steward <scala_steward@virtuslab.com>
2023-02-10 17:27:15 +0000

    Update circe-generic to 0.14.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val finchVersion = "0.31.0"
-  lazy val circeVersion = "0.12.3"
+  lazy val circeVersion = "0.14.4"
 
   lazy val finch = "com.github.finagle" %% "finchx-core" % finchVersion
   lazy val finchCirce = "com.github.finagle" %% "finchx-circe" % finchVersion
